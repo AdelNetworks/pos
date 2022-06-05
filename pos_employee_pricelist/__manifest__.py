3cb0fb873cf8f764c604fbe6347b403fdc294688
Adel Beltran <adelbeltran03@gmail.com>
2022-06-05 03:56:37 -0400

    POS LISTA DE PRECIO Y PISOS

---
+++ pos_employee_pricelist/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'pos employee pricelist',
+    'version': '1.0',
+    'category': 'Sales/Point of Sale',
+    'summary': 'pos employee pricelist',
+    'description': 'pos employee pricelist',
+    'website': 'http://warlocktechnologies.com',
+    'depends': ['point_of_sale','pos_hr'],
+    'data': [
+        'views/assets.xml',
+        'views/views.xml'
+    ],
+    'images': [],
+    'license': 'AGPL-3',
+    'installable': True,
+}
